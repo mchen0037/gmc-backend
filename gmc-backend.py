adfa154c431bc4a8c7051b49d06b4a11ee3599dc
Mighty Chen <mchen73@ucmerced.edu>
2018-11-18 08:31:21 -0800

    put together with glue and tape

--- gmc-backend.py
+++ gmc-backend.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from flask import request
 import subprocess
 import os
+from os import listdir
 import pandas
 from pandas import Series
 import rpy2.robjects as ro
@@ -17,11 +18,6 @@ def createModel(user):
     r_query = "dat = read.csv(\'" + path + "\')"
     dat = ro.r(r_query)
     ro.r("""attach(dat)""")
-    # print(ro.r("""summary(dat)"""))
-    # print(ro.r("""names(dat)"""))
-    # print(ro.r("""acousticness"""))
-
-
     # this comment is to commemorate the 1 hour you spent frekaing out at R because you literally
     # passed the same in the good and bad data from your front end... holy..
 
@@ -31,15 +27,31 @@ def createModel(user):
     data=dat,
     family='binomial') """)
 
-    # print(model)
-
     ro.r("""
     save(logModelA, file = "models/""" + str(user) + """.rda")
     """)
 
-def predict():
-    ro.r("""load('/home/mighty/gmc/gmc-backend/models/')""")
-    print(ro.r("""model"""))
+def prediction(user):
+    # user = "nothing_faith"
+    file = """load('/home/mighty/gmc/gmc-backend/models/""" + user + """.rda')"""
+    test = """read.csv('/home/mighty/gmc/gmc-backend/models/test.csv')"""
+
+    ro.r("""models = """ + file)
+    print(ro.r("""logModelA"""))
+    ro.r("""new_dat = """ + test)
+
+    ro.r("""logModel.probs = predict(logModelA, new_dat, type='response')""")
+    # print(ro.r("""logModel.probs"""))
+    ro.r("""logModel.preds=rep('bad', dim(new_dat)[1])""")
+    ro.r("""logModel.preds[logModel.probs > 0.5]='good'""")
+    c = ro.r("""logModel.preds""")
+
+    list = []
+    for x in c:
+        list.append(x)
+
+    return list
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret'
@@ -52,20 +64,17 @@ def train():
     GOOD_AUDIO_FEATURES = request.get_json()["good"]
     BAD_AUDIO_FEATURES = request.get_json()["bad"]
 
-    # print(type(GOOD_AUDIO_FEATURES))
-    # print(GOOD_AUDIO_FEATURES)
-
     # this is so janky please fix this lol
     df = pandas.DataFrame(eval(str(GOOD_AUDIO_FEATURES)))
     col = []
     for x in range(df.shape[0]):
-        col.append(1)
+        col.append('good')
     df['class'] = Series(col)
 
     df2 = pandas.DataFrame(eval(str(BAD_AUDIO_FEATURES)))
     col = []
     for x in range(df2.shape[0]):
-        col.append(0)
+        col.append('bad')
     df2['class'] = Series(col)
 
     frames = [df, df2]
@@ -78,10 +87,41 @@ def train():
     # do something with the audio features. i.e: Call RPy2 and train model.
     return jsonify({"result": True})
 
-@app.route("/predict/<string:user", methods=['GET'])
-def predict():
-    print('hello')
-    return('yo')
+@app.route("/models/<string:user>", methods=['GET'])
+def models(user):
+    # print(root + "/models/" + user + ".rda")
+    fileExists = (os.path.isfile(root + "/models/" + user + ".rda"))
+    return jsonify({"result": fileExists})
+
+@app.route("/delete/<string:user>", methods=['GET'])
+def delete(user):
+    os.remove(root + "/models/" + user + ".rda")
+    os.remove(root + "/models/" + user + ".csv")
+    return jsonify({"result": True})
+
+@app.route("/all", methods=['GET'])
+def all():
+    l = os.listdir()
+    print(l)
+    return ('yo')
+
+@app.route("/predict/<string:user>", methods=['POST'])
+def predict(user):
+    print(user)
+    AUDIO_FEATURES = request.get_json()["test"]
+
+    df = pandas.DataFrame(eval(str(AUDIO_FEATURES)))
+    col = []
+    for x in range(df.shape[0]):
+        col.append('bad')
+    df['class'] = Series(col)
+
+    df.to_csv('models/test.csv')
+
+    res = prediction(user)
+    print(res)
+
+    return jsonify({"results": res})
 
 
 # localhost:4000/getusername is the url
