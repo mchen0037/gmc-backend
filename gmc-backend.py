eda025b6583fa0682197cc566b82c691f05087e1
Mighty Chen <mchen73@ucmerced.edu>
2018-11-18 05:10:07 -0800

    handled model creation in backend (no db)

--- gmc-backend.py
+++ gmc-backend.py
@@ -5,11 +5,84 @@ from flask import jsonify
 from flask import request
 import subprocess
 import os
+import pandas
+from pandas import Series
+import rpy2.robjects as ro
+
+root = "/home/mighty/gmc/gmc-backend"
+
+def createModel(user):
+    print(user)
+    path = root + '/models/' + str(user) + '''.csv'''
+    r_query = "dat = read.csv(\'" + path + "\')"
+    dat = ro.r(r_query)
+    ro.r("""attach(dat)""")
+    # print(ro.r("""summary(dat)"""))
+    # print(ro.r("""names(dat)"""))
+    # print(ro.r("""acousticness"""))
+
+
+    # this comment is to commemorate the 1 hour you spent frekaing out at R because you literally
+    # passed the same in the good and bad data from your front end... holy..
+
+    model = ro.r("""
+    logModelA =
+    glm(class~danceability+energy+key+loudness+mode+speechiness+acousticness+instrumentalness+liveness+valence+tempo+duration_ms+time_signature,
+    data=dat,
+    family='binomial') """)
+
+    # print(model)
+
+    ro.r("""
+    save(logModelA, file = "models/""" + str(user) + """.rda")
+    """)
+
+def predict():
+    ro.r("""load('/home/mighty/gmc/gmc-backend/models/')""")
+    print(ro.r("""model"""))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret'
 # //cross origin request -security stuff
 CORS(app)
+
+@app.route("/train", methods=['POST'])
+def train():
+    USER = request.get_json()["user"]
+    GOOD_AUDIO_FEATURES = request.get_json()["good"]
+    BAD_AUDIO_FEATURES = request.get_json()["bad"]
+
+    # print(type(GOOD_AUDIO_FEATURES))
+    # print(GOOD_AUDIO_FEATURES)
+
+    # this is so janky please fix this lol
+    df = pandas.DataFrame(eval(str(GOOD_AUDIO_FEATURES)))
+    col = []
+    for x in range(df.shape[0]):
+        col.append(1)
+    df['class'] = Series(col)
+
+    df2 = pandas.DataFrame(eval(str(BAD_AUDIO_FEATURES)))
+    col = []
+    for x in range(df2.shape[0]):
+        col.append(0)
+    df2['class'] = Series(col)
+
+    frames = [df, df2]
+    data = pandas.concat(frames, sort=False)
+
+    data.to_csv('models/' + str(USER) + '.csv')
+
+    createModel(USER)
+
+    # do something with the audio features. i.e: Call RPy2 and train model.
+    return jsonify({"result": True})
+
+@app.route("/predict/<string:user", methods=['GET'])
+def predict():
+    print('hello')
+    return('yo')
+
 
 # localhost:4000/getusername is the url
 # This route can recieve get requests or post requests
@@ -18,21 +91,23 @@ CORS(app)
 # http://localhost:4000/username?id=1 args is whatever is after the questionmark
 
 # http://localhost:4000/username/1
-@app.route("/username/<int:id>", methods = ['GET'])
-def getUser(id):
-    # print(request.args.get('id'))
-    print(id)
-    # return jsonify({"username": "yoboimightychen", "id": request.args.get('id')});
-    return jsonify({"username": "yoboimightychen", "id": id})
+# @app.route("/username", methods = ['POST'])
+# def getUser():
+#     # print(request.args.get('id'))
+#     userID = request.get_json()["userID"]
+#     print(request.get_json()["userID"])
+#
+#     # return jsonify({"username": "yoboimightychen", "id": request.args.get('id')});
+#     # jsonify({"username": "yoboimightychen", "id": id}
+#     return
 
 
-@app.route("/login", methods = ['POST'])
-@cross_origin()
-def login():
-    print(request.json)
-
-
-    return jsonify(request.json)
+# @app.route("/login", methods = ['POST'])
+# def login():
+#     print(request.get_json()["userID"])
+#
+#     return "got stuff"
+#     # return jsonify(request.json)
 
 
 # @app.route("/createuser")
